400eff8e0e0c087003aa8c47b3a959065c072c76
sane4ek24 <119444641+sane4ek24@users.noreply.github.com>
2024-07-04 20:53:43 +0500

    first commit

---
+++  Переменные/homework2.py
@@ -0,0 +1,6 @@
+completed_tasks = 12
+hours_spent = 1.5
+course_title = "Python"
+time_per_job = 0.125
+print(course_title, ', всего задач:', completed_tasks, ', затрачено часов:', hours_spent, ', среднее время выполнения',
+      time_per_job)
