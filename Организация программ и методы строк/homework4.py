b55583aa52c35512643d4da64bbfaadf8444963c
sane4ek24 <119444641+sane4ek24@users.noreply.github.com>
2024-07-05 01:40:33 +0500

    homework4.py commit

---
+++ Организация программ и методы строк/homework4.py
@@ -0,0 +1,8 @@
+my_string = (input("Напишите что нибудь: "))
+string = len(my_string)
+print("Количество символов в вашей строке: ", string)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
