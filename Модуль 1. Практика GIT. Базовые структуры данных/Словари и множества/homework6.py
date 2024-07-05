7bd6c0c14fc467cc0db484ad7a1a7b123b356ec1
sane4ek24 <119444641+sane4ek24@users.noreply.github.com>
2024-07-05 19:54:31 +0500

    test commit 5

---
+++ Модуль 1. Практика GIT. Базовые структуры данных/Словари и множества/homework6.py
@@ -0,0 +1,15 @@
+my_dict = {'Oleg': 1995, 'Stas': 1996, 'Boris': 1985}
+print(my_dict)
+print(my_dict.get('Stas'))
+print(my_dict.get('Sasha', 'Такого имени нет'))
+my_dict.update({'Sasha': 1990, 'Sergey': 1992})
+print(my_dict)
+del my_dict['Oleg']
+print(my_dict)
+
+my_set = {1, 2, 3, 3, 5, True, 'str', True}
+print(my_set)
+my_set.add(4)
+my_set.add(False)
+my_set.discard(3)
+print(my_set)
