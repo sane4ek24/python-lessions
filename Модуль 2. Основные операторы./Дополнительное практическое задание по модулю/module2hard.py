6e092bfffb131fcb39a76390743e2a5d0cdfb87b
sane4ek24 <119444641+sane4ek24@users.noreply.github.com>
2024-07-15 20:06:41 +0500

    module2hard.py commit

---
+++ Модуль 2. Основные операторы./Дополнительное практическое задание по модулю/module2hard.py
@@ -0,0 +1,17 @@
+import random
+
+first_number = random.randint(3, 20)
+result = []
+print(f'Число в первой вставке: {first_number}')
+for i in range(1, 21):
+    for j in range(1, 21):
+        if i == j:
+            continue
+        if first_number % (i + j) == 0 and j > i:
+            result.append(i)
+            result.append(j)
+result1 = ''
+for p in result:
+    result1 += str(p)
+
+print('Шифр:', result1)
