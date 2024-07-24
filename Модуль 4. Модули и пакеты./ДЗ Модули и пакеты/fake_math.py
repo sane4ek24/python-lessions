2c5342f3765fbea1d456df9b51e072178c6cbae6
sane4ek24 <119444641+sane4ek24@users.noreply.github.com>
2024-07-25 01:39:09 +0500

    module_4_1.py commit

---
+++ Модуль 4. Модули и пакеты./ДЗ Модули и пакеты/fake_math.py
@@ -0,0 +1,8 @@
+from math import inf
+
+
+def f_divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        return first / second
---
+++ Модуль 4. Модули и пакеты./ДЗ Модули и пакеты/true_math.py
@@ -0,0 +1,5 @@
+def t_divide(first, second):
+    if second == 0:
+        return "Ошибка"
+    else:
+        return first / second
