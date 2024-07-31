0e3c12bfbd9e959a592e13df76f75c9233afc95e
sane4ek24 <119444641+sane4ek24@users.noreply.github.com>
2024-08-01 02:06:54 +0500

    module_5_4.py.py commit

---
+++ Модуль 5. Классы и объекты./Различие атрибутов класса и экземпляра/module_5_4.py
@@ -0,0 +1,61 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        obj = super().__new__(cls)
+        cls.houses_history.append(args[0])  # добавляем название строения в историю
+        return obj
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        return House(self.name, self.number_of_floors + value)
+
+    def __radd__(self, value):
+        return House(self.name, self.number_of_floors + value)
+
+    def __iadd__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
